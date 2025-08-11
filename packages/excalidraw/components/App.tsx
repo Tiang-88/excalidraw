39a04e658e76d83cc29dbc23240bd71f1a82174f
zsviczian <viczian.zsolt@gmail.com>
2025-08-11 11:52:44 +0200

    fix: do not auto-add elements to locked frame (#9851)
    * Do not return locked frames when filtering for top level frame
    
    * lint
    
    * lint
    
    * lint

--- packages/excalidraw/components/App.tsx
+++ packages/excalidraw/components/App.tsx
@@ -5735,8 +5735,9 @@ class App extends React.Component<AppProps, AppState> {
     const elementsMap = this.scene.getNonDeletedElementsMap();
     const frames = this.scene
       .getNonDeletedFramesLikes()
-      .filter((frame): frame is ExcalidrawFrameLikeElement =>
-        isCursorInFrame(sceneCoords, frame, elementsMap),
+      .filter(
+        (frame): frame is ExcalidrawFrameLikeElement =>
+          !frame.locked && isCursorInFrame(sceneCoords, frame, elementsMap),
       );
 
     return frames.length ? frames[frames.length - 1] : null;
